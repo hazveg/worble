b61fb1f65ad896175acc501484ae5344a73cd05b
hazveg <aaron.jaeger06@gmail.com>
2025-12-04 10:37:28 +0100

    added unigram generation + did little refactors
    i propose that we pursue n-gram language models as described in
    https://web.stanford.edu/~jurafsky/slp3/3.pdf. while usually used for
    words in a sentence, it can easily be re-oriented to letters in words.
    because all words are to be a fixed length, that should significantly
    simplify statistical model that comes out. more than likely it'd be a
    5- or 4-gram - depending on how the amount of preceeding letters works
    out in context. for tomorrow i'll provide an implementation using a
    unigram for now. a unigram just disregards all previous context letters
    and works solely with basic relative frequency of the letters.

--- worble_hax/worble_hax.py
+++ worble_hax/worble_hax.py
@@ -7,13 +7,28 @@ SRC = "srcer.txt"
 DEFINED_KEYWORD = "def"
 CONTAINED_KEYWORD = "con"
 
-intel = { }
+VALID_CHARS = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ä', 'ö', 'ü' ]
 
-def keyname(char, keyw, idx):
+src_str = ""
+words = []
+info_indices = {}
+unigram = {}
+
+def source_words():
+    # goofy ahh language
+    global src_str
+    global words
+
+    file = open(SRC, "r")
+
+    src_str = file.read()
+    words = src_str.split()
+
+def info_keyname(char, keyw, idx):
     return f"{char}_{keyw}_{idx}"
 
-def derive_information(word):
-    char_idx = 0
+def derive_info(idx, word):
+    global info_indices
     nth_occurence = {
         'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0,
         'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0,
@@ -24,25 +39,65 @@ def derive_information(word):
     }
 
     chars = list(word)
-    for char in chars:
-        def_key = keyname(char, DEFINED_KEYWORD, char_idx)
-        char_idx += 1
-        con_key = keyname(char, CONTAINED_KEYWORD, nth_occurence[char])
+    for char_idx, char in enumerate(chars):
+        def_key = info_keyname(char, DEFINED_KEYWORD, char_idx)
+        con_key = info_keyname(char, CONTAINED_KEYWORD, nth_occurence[char])
         nth_occurence[char] += 1
 
-        if not def_key in intel:
-            intel[def_key] = []
-        if not con_key in intel:
-            intel[con_key] = []
+        if not def_key in info_indices:
+            info_indices[def_key] = []
+        if not con_key in info_indices:
+            info_indices[con_key] = []
 
-        intel[def_key].append(word)
-        intel[con_key].append(word)
+        info_indices[def_key].append(idx)
+        info_indices[con_key].append(idx)
 
-file = open(SRC, "r")
-words = file.read().split()
+def generate_info_indices():
+    for idx, word in enumerate(words):
+        derive_info(idx, word)
 
-for word in words:
-    derive_information(word)
+def get_num_occurences_for_char(char):
+    num_occurences = 0
+    for src_char in list(src_str):
+        if src_char != char:
+            continue
+        
+        num_occurences += 1
 
-print(list(intel.keys()))
-print(intel["x_con_0"])
+    return num_occurences
+
+def generate_unigram():
+    global unigram
+
+    total_num_words = len(words)
+    # since we can guarantee that all words in the source list are 5 letters 
+    # long, we can spare ourselves the headache of counting each char 
+    # *individually*.
+    total_num_letters = 5 * total_num_words
+
+    # print(f"total_num_letters: {total_num_letters}")
+    for char in VALID_CHARS:
+        num_occurences = get_num_occurences_for_char(char)
+        char_probability = num_occurences / total_num_letters # unigram is basically relative probability only
+
+        unigram[char] = char_probability
+
+        print(f"num_occurences for '{char}': {num_occurences}")
+        print(f"probability for '{char}': {round(char_probability, 2)}")
+
+def main():
+    source_words()
+    generate_info_indices()
+
+    """
+    print(list(INFO_INDICES.keys()))
+
+    for idx in INFO_INDICES["x_con_0"]:
+        print(WORDS[idx])
+    """
+
+    generate_unigram()
+    # print(list(unigram.keys()))
+
+if __name__ == "__main__":
+    main()
