555ea17214f821d0923eb1d0e571e8fe39516e70
hazveg <aaron.jaeger06@gmail.com>
2025-12-07 17:52:26 +0100

    implemented slotgram
    it's a bigram of a context and one of the 29 valid chars. not the best
    solution, the context is represented by a string with one of the chars
    replaced with an '_'. not the path i wanna take, especially when the
    basic fivegram comes about. i'll think of something.
    
    the main problem is that different words can generate the same context.
    as such, there's some deduplication that needs to be done such as to
    allow game datasets other than the training set to be workable with the
    slotgram. unfortunately, any method that doesn't just leverage setting a
    string as a key in a dict and then playing with the weights from there
    doesn't work without an amount of overhead from my own derivations.

--- worble_hax/worble_hax.py
+++ worble_hax/worble_hax.py
@@ -6,6 +6,7 @@ SRC = "srcer.txt"
 
 DEFINED_KEYWORD = "def"
 CONTAINED_KEYWORD = "con"
+NOTDEFINED_KEYWORD = "ndef"
 
 VALID_CHARS = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ä', 'ö', 'ü' ]
 
@@ -13,6 +14,8 @@ src_str = ""
 words = []
 info_indices = {}
 unigram = {}
+fivegram = {}
+slotgram = {}
 
 def source_words():
     # goofy ahh language
@@ -52,6 +55,17 @@ def derive_info(idx, word):
         info_indices[def_key].append(idx)
         info_indices[con_key].append(idx)
 
+    for char in nth_occurence.keys():
+        if nth_occurence[char] != 0:
+            continue
+        
+        ndef_key = info_keyname(char, NOTDEFINED_KEYWORD, 0)
+
+        if not ndef_key in info_indices:
+            info_indices[ndef_key] = []
+
+        info_indices[ndef_key].append(idx)
+
 def generate_info_indices():
     for idx, word in enumerate(words):
         derive_info(idx, word)
@@ -85,33 +99,137 @@ def generate_unigram():
         # print(f"num_occurences for '{char}': {num_occurences}")
         # print(f"probability for '{char}': {round(char_probability, 2)}")
 
+def generate_fivegram():
+    global fivegram
+    print("TODO: implement fivegram generation")
+
+# this function returns a new string where the character at idx in str 
+# was replaced with char.
+def replace_char_in_str(str, idx, char):
+    tmp = list(str)
+    tmp[idx] = char
+    res = "".join(tmp)
+    return res
+
+# not a fan of using strings as the keys, but with dictionaries this is the 
+# only way, with proper structs i'd create a tree structure where two noes 
+# share a child node.
+def define_slotgram_contexts():
+    global slotgram
+    global words
+
+    for word in words:
+        chars = list(word)
+
+        for slot_idx in range(5):
+            # generate context_str and ensure that there are only unique 
+            # entries, initialize empty dictionary for each slotgram.
+            context_str = replace_char_in_str(word, slot_idx, '_')
+            slotgram[context_str] = {}
+
+def generate_slotgram():
+    global slotgram
+    global words
+    
+    define_slotgram_contexts()
+
+    total_num_contexts = len(slotgram)
+    for context in slotgram.keys():
+        slot_idx = context.index('_')
+        context_freq = 0
+        for char in VALID_CHARS:
+            # for now unigram will store absolute freq of char in context
+            slotgram[context][char] = 0
+            tmp = replace_char_in_str(context, slot_idx, char)
+
+            for word in words:
+                if word != tmp:
+                    continue
+
+                slotgram[context][char] += 1
+                context_freq += 1
+        
+        # normalize char <-> context weights
+        # print(f"before: {slotgram[context_str]}")
+        for char in VALID_CHARS:
+            slotgram[context][char] /= context_freq
+            slotgram[context][char] /= total_num_contexts
+        # print(f"after: {slotgram[context_str]}")
+
+def validate_unigram_weights():
+    global unigram
+
+    total = 0.0
+    for char in unigram.keys():
+        total += unigram[char]
+
+    print(total)
+
+def validate_fivegram_weights():
+    global fivegram
+    print("TODO: implement fivegram")
+
+def validate_slotgram_weights():
+    global slotgram
+
+    total = 0.0
+    for context in slotgram.keys():
+        for char in slotgram[context].keys():
+            total += slotgram[context][char]
+
+    print(total)
+
 def init():
     source_words()
     generate_info_indices()
     generate_unigram()
+    # validate_unigram_weights()
+    # generate_fivegram()
+    generate_slotgram()
+    # validate_slotgram_weights()
 
 def determine_array_intersection(a, b):
     return list(set(a) & set(b))
 
 def P_unigram(char):
+    global unigram
     return unigram[char]
+
+def P_fivegram(char):
+    global fivegram
+    print("TODO: implement fivegram")
+    return 0.0
+
+def P_slotgram(word, slot_idx):
+    global slotgram
+    char = word[slot_idx]
+    context = replace_char_in_str(word, slot_idx, '_')
+    return slotgram[context][char]
 
 def determine_word_probability(word):
     probability = 1.0
     chars = list(word)
     
-    for char in chars:
+    for char_idx, char in enumerate(chars):
         # we don't need to worry quite yet i think, but moving these numbers 
         # - either at base or at least here - into "log space" would afford 
         # us better numerical stability. the resulting probability we get 
         # from this function ends up in the 10^-[6;9] area. it'd certainly 
         # be more like people usually handle language models
-        probability *= P_unigram(char)
+        # probability *= P_unigram(char)
+        probability *= P_slotgram(word, char_idx)
 
     return probability
 
 def limit_search_area_with_infos(infos):
     applicable_word_indices = []
+    
+    # if no infos are provided, generate a list of indices for the whole word
+    # list. 06.12.2025: FUCK I FORGOT TO PUSH THIS FOR THE DEMO
+    if len(infos) == 0:
+        applicable_word_indices = range(len(words))
+        return applicable_word_indices
+
     for idx, info in enumerate(infos):
         if idx == 0:
             applicable_word_indices = info_indices[info]
@@ -149,7 +267,7 @@ def determine_most_likely_next_word(infos):
 
 def main():
     init()
-    test = [ "p_def_0", "e_con_0" ]
+    test = [ "e_ndef_0", "d_def_0" ]
     word_index = determine_most_likely_next_word(test)
     print(words[word_index])
 
