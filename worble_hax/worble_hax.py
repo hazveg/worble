3beff8e54abc0e7614b3e120a73ae4c42c9c0ca7
hazveg <aaron.jaeger06@gmail.com>
2025-12-09 10:29:46 +0100

    implemented model dumping, dropped fivegram for trigram
    no amount of data i shoot into the fivegram - and more notably the
    slotgram makes them less shit, so i've opted to reduce the context for
    there to be any variance in likelihood with which we can make
    estimations. i modified the fivegram into a trigram because their logic
    for generation is similar. it works pretty good, especially once you
    provide a good corpus.
    
    we can now dump the different models into a text file. for now it's just
    outputting to stdout to be redirected into a file; but that'll change in
    the next patch. the format is similar to doom wad files, just in
    plaintext rather than in binary - easier, more intuitive handling for
    umlauts, especially in a python context. there's a header that denotes
    the model version and redirects you to a directory for the different
    kinds of data it can store. how the data is interpreted is contingent on
    the implementation for that lump type e.g. UNIGRAM or TRIGRAM_INDEX.
    
    i'll split worble_hax into two modules, one that only does the
    generation of the models, and the other that does the probability
    calculations to be converted into the service.

--- worble_hax/worble_hax.py
+++ worble_hax/worble_hax.py
@@ -4,13 +4,15 @@
 
 import math
 
-SRC = "srcer.txt"
+SRC = "srcester.txt"
 
 DEFINED_KEYWORD = "def"
 CONTAINED_KEYWORD = "con"
 NOTDEFINED_KEYWORD = "ndef"
 
 SLOT_CONTROLCHAR = '_'
+
+HEADER_STR = "WORBLE_HAX_MODEL_0"
 
 VALID_CHARS = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ä', 'ö', 'ü' ]
 
@@ -21,8 +23,8 @@ info_model = {}
 
 unigram = {}
 
-fivegram_index = {}
-fivegram_weights = []
+trigram_index = {}
+trigram_weights = []
 
 slotgram_index = {}
 slotgram_weights = []
@@ -105,61 +107,69 @@ def generate_unigram():
 
         unigram[char] = char_probability
 
-def define_fivegram_contexts():
-    global fivegram_index
+def define_trigram_contexts():
+    global trigram_index
     global words
 
     required_context_sequence = {}
     context_rover = 0
     for word_idx, word in enumerate(words):
-        for context_len in range(5):
-            context_str = word[0:context_len]
+        chars = list(word)
+        for char_idx, char in enumerate(chars):
+            context_str = ""
+            context_len = min(char_idx, 2)
+            if context_len != 0:
+                context_str = word[char_idx - context_len:char_idx]
 
             if not context_str in required_context_sequence:
                 required_context_sequence[context_str] = context_rover
                 context_rover += 1
 
             context_idx = required_context_sequence[context_str]
-            fivegram_index[(word_idx, context_len)] = context_idx
-
+            trigram_index[(word_idx, char_idx)] = context_idx
+    
     return required_context_sequence
 
-def FIVEGRAM_set_context_abs_freqs(context, context_weights):
+def set_trigram_context_abs_freqs(context, context_weights):
     context_freq = 0
     for char in VALID_CHARS:
         # for now we'll store the absolute frequency
         context_weights[char] = 0
+        if len(context) == 0:
+            context_weights[char] = unigram[char]
+            context_freq = 1
+            continue
+
         tmp = context + char
         # we limit the search area to words that have the context's first 
         # char defined
-        info = info_keyname(tmp[0], DEFINED_KEYWORD, 0)
+        info = info_keyname(tmp[0], CONTAINED_KEYWORD, 0)
         search_area = limit_search_area_with_info(info)
 
         for word_idx in search_area:
             word = words[word_idx]
-            if not word.startswith(tmp):
+            if not tmp in word:
                 continue
 
             context_weights[char] += 1
             context_freq += 1
-
+    
     return context_freq
 
-
-def generate_fivegram():
-    global fivegram_weights
+def generate_trigram():
+    global trigram_weights
     global words
 
-    required_context_sequence = define_fivegram_contexts()
+    required_context_sequence = define_trigram_contexts()
     total_num_contexts = len(required_context_sequence)
     # init each context as a dict uniquely instead of `total_num_contexts` 
     # copies of the same blank dictionary
-    fivegram_weights = [{} for i in range(total_num_contexts)]
+    trigram_weights = [{} for i in range(total_num_contexts)]
     
     for context, weights_idx in required_context_sequence.items():
-        context_weights = fivegram_weights[weights_idx]
+        context_weights = trigram_weights[weights_idx]
 
-        context_freq = FIVEGRAM_set_context_abs_freqs(context, context_weights)
+        context_freq = set_trigram_context_abs_freqs(context, context_weights)
         normalize_context_weights(context_weights, context_freq)
 
 # this function returns a new string where the character at idx in str 
@@ -241,14 +251,154 @@ def generate_slotgram():
         context_freq = set_context_abs_freqs(context, slot_idx, context_weights)
         normalize_context_weights(context_weights, context_freq)
 
+def load_unigram(file_lines, start, end):
+    global unigram
+
+    for line_idx in range(start, end + 1):
+        line = file_lines[line_idx]
+        line_cols = line.split()
+
+        char = line_cols[0]
+        weight = float(line_cols[1])
+
+        unigram[char] = weight
+
+def load_trigram_index(file_lines, start, end):
+    global trigram_index
+
+    for line_idx in range(start, end):
+        line = file_lines[line_idx]
+        line_cols = line.split()
+
+        tup_0 = int(line_cols[0])
+        tup_1 = int(line_cols[1])
+        weights_idx = int(line_cols[2])
+        
+        trigram_index[(tup_0, tup_1)] = weights_idx
+
+def load_trigram_weights(file_lines, start, end):
+    global trigram_weights
+
+    context_weights = {}
+    for line_idx in range(start, end):
+        line = file_lines[line_idx]
+        line_cols = line.split()
+
+        char = line_cols[0]
+        weight = float(line_cols[1])
+
+        context_weights[char] = weight
+        if (len(context_weights) >= len(VALID_CHARS)):
+            trigram_weights.append(context_weights)
+            context_weights = {}
+
+def load_model(file):
+    file = open(file, "r")
+    file_str = file.read()
+    lines = file_str.split('\n')
+    
+    header = lines[0]
+    header_cols = header.split()
+    header_string = header_cols[0]
+    directory_start = int(header_cols[1])
+    if (header_string != HEADER_STR):
+        # wrong version
+        return
+    
+    print(f"header_string: {header_string}, directory_start: {directory_start}")
+    dir_entries = []
+    for directory_entry_idx in range(directory_start, len(lines)):
+        dir_entry = lines[directory_entry_idx]
+        if (len(dir_entry) == 0):
+            continue
+
+        print(dir_entry)
+        dir_entries.append(dir_entry)
+
+    for dir_entry in dir_entries:
+        dir_entry_cols = dir_entry.split()
+        dir_entry_name = dir_entry_cols[0]
+        dir_entry_start_line = int(dir_entry_cols[1])
+        dir_entry_closing_line = int(dir_entry_cols[2])
+
+        print(f"dir_entry_name: {dir_entry_name}, dir_entry_start_line: {dir_entry_start_line}, dir_entry_closing_line: {dir_entry_closing_line}")
+
+        match dir_entry_name:
+            case "UNIGRAM":
+                print("processing unigram")
+                load_unigram(lines, dir_entry_start_line, dir_entry_closing_line)
+            case "TRIGRAM_INDEX":
+                print("processing trigram index")
+                load_trigram_index(lines, dir_entry_start_line, dir_entry_closing_line)
+            case "TRIGRAM_WEIGHTS":
+                print("processing trigram weights")
+                load_trigram_weights(lines, dir_entry_start_line, dir_entry_closing_line)
+            case _:
+                # invalid directory entry
+                continue
+
 def init():
     source_words()
 
     generate_info_model()
+    
+    load_model("worble_hax")
+    # generate_unigram()
+    # generate_trigram()
+    # generate_slotgram()
 
-    generate_unigram()
-    generate_fivegram()
-    generate_slotgram()
+
+def dump_unigram(file_lines):
+    global unigram
+    
+    starting_line = len(file_lines)
+    for char, weight in unigram.items():
+        file_lines.append(f"{char} {weight}\n")
+    closing_line = len(file_lines)
+    return f"UNIGRAM {starting_line} {closing_line}\n"
+
+def dump_trigram_index(file_lines):
+    global trigram_index
+    
+    starting_line = len(file_lines)
+    for tup, idx in trigram_index.items():
+        file_lines.append(f"{tup[0]} {tup[1]} {idx}\n")
+    closing_line = len(file_lines)
+    return f"TRIGRAM_INDEX {starting_line} {closing_line}\n"
+
+def dump_trigram_weights(file_lines):
+    global trigram_weights
+
+    starting_line = len(file_lines)
+    for context in trigram_weights:
+        for char, weight in context.items():
+            file_lines.append(f"{char} {weight}\n")
+    closing_line = len(file_lines)
+    return f"TRIGRAM_WEIGHTS {starting_line} {closing_line}\n"
+
+def dump_model():
+    file_lines = []
+    dir_entries = []
+    
+    # we'll set the header later
+    file_lines.append("")
+    dir_entries.append(dump_unigram(file_lines))
+    dir_entries.append(dump_trigram_index(file_lines))
+    dir_entries.append(dump_trigram_weights(file_lines))
+
+    directory_start = len(file_lines)
+    file_lines[0] = f"{HEADER_STR} {directory_start}\n"
+    for entry in dir_entries:
+        file_lines.append(entry)
+    
+    file_str = ""
+    for line in file_lines:
+        file_str += line
+
+    print(file_str)
+
+def destroy():
+    dump_model()
 
 def determine_array_intersection(a, b):
     return list(set(a) & set(b))
@@ -257,13 +407,13 @@ def P_unigram(char):
     global unigram
     return unigram[char]
 
-def P_fivegram(word_idx, context_len):
-    global fivegram_index
-    global fivegram_weights
+def P_trigram(word_idx, char_idx):
+    global trigram_index
+    global trigram_weights
 
-    char = words[word_idx][context_len]
-    context_weights_idx = fivegram_index[(word_idx, context_len)]
-    return fivegram_weights[context_weights_idx][char]
+    char = words[word_idx][char_idx]
+    context_weights_idx = trigram_index[(word_idx, char_idx)]
+    return trigram_weights[context_weights_idx][char]
 
 def P_slotgram(word_idx, slot_idx):
     global slotgram_index
@@ -284,8 +434,8 @@ def determine_word_probability(word_idx):
         # from this function ends up in the 10^-[6;24] area. it'd certainly 
         # be more like people usually handle language models.
         # probability *= P_unigram(char)
-        # probability *= P_fivegram(word_idx, char_idx)
-        probability *= P_slotgram(word_idx, char_idx)
+        probability *= P_trigram(word_idx, char_idx)
+        # probability *= P_slotgram(word_idx, char_idx)
 
     return probability
 
@@ -318,9 +468,11 @@ def rank_words(word_indices):
     # lists don't sort implicitely apparently
     # unfortunately there is no reverse order sorting
     ranked_words.sort()
-
+    
+    """
     for rank in ranked_words:
         print(f"{rank}: {rank[1]} = { words[rank[1]] }")
+    """
 
     return ranked_words
 
@@ -336,10 +488,11 @@ def determine_most_likely_next_word(infos):
 
 def main():
     init()
-    test = [ "e_ndef_0", "d_def_0" ]
-    # test = []
+    # test = [ "e_ndef_0", "d_def_0" ]
+    test = []
     word_index = determine_most_likely_next_word(test)
     print(words[word_index])
+    # destroy()
 
 if __name__ == "__main__":
     main()
